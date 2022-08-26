6936b3da845d6e2a258b1c55261f260d34675472
Lewis Thorley <lewis.thorley@netlify.com>
2022-08-26 10:07:48 +0100

    Initial commit for emails plugin

--- node_modules/@netlify/edge-bundler/dist/types.d.ts
+++
@@ -1,4 +0,0 @@
-import type { DenoBridge } from './bridge.js';
-import type { Logger } from './logger.js';
-declare const ensureLatestTypes: (deno: DenoBridge, logger: Logger, customTypesURL?: string) => Promise<void>;
-export { ensureLatestTypes };
