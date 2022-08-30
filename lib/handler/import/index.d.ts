a81d2a7ab9793aa894f412483f8f0508ae617b50
Lewis Thorley <lewis.thorley@netlify.com>
2022-08-30 10:42:00 +0100

    Pre commit removed

---
+++ lib/handler/import/index.d.ts
@@ -0,0 +1,2 @@
+import { handler } from "@netlify/plugin-emails";
+export { handler };
---
+++ lib/handler/index.d.ts
@@ -0,0 +1,4 @@
+import { Handler } from "@netlify/functions";
+export declare const getEmailFromPath: (path: string) => undefined;
+declare const handler: Handler;
+export { handler };
