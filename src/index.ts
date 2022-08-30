b1768efcdf9802449bdce786de2776bd3d1f0ba6
Lewis Thorley <lewis.thorley@netlify.com>
2022-08-30 10:51:42 +0100

    Add recursive back

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const onPreBuild = async ({
 }: {
   constants: { FUNCTIONS_SRC: string };
 }) => {
-  fs.mkdirSync("./netlify/functions/email/");
+  fs.mkdirSync("./netlify/functions/email/", { recursive: true });
   // This is a real hack and there may be a better way to inject a function!
   fs.writeFileSync(
     `./netlify/functions/email/index.ts`,
