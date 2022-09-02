5bf60c3da9a3f32cc8e34c4d7ad34366035a2e74
Lewis Thorley <lewis.thorley@netlify.com>
2022-09-02 11:55:46 +0100

    Add to and from and update regex

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -4,7 +4,7 @@ import Handlebars from "handlebars";
 import { ServerClient } from "postmark";
 
 export const getEmailFromPath = (path: string) => {
-  console.log(`Getting template for: ${path}`);
+  console.log(`Getting the template for: ${path}`);
 
   let fileContents: string | undefined = undefined;
   fs.readdirSync(path).forEach((file) => {
@@ -26,24 +26,43 @@ export const getEmailFromPath = (path: string) => {
 };
 
 const handler: Handler = async (event, _) => {
-  const emailPath = event.rawUrl.match(/email\/([A-z]*)[\?]?/)?.[1];
+  const emailPath = event.rawUrl.match(/email\/([A-z-]*)[\?]?/)?.[1];
+  const params = event.queryStringParameters;
   if (!emailPath) {
     return {
-      statusCode: 500,
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "No email path provided",
+      }),
+    };
+  }
+  if (!params?._from) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "From address is required",
+      }),
+    };
+  }
+  if (!params?._to) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Too address is required",
+      }),
     };
   }
   const fullEmailPath = `./emails/${emailPath}`;
-  const params = event.queryStringParameters;
   const fileContents = getEmailFromPath(fullEmailPath);
   const template = Handlebars.compile(fileContents);
   const renderedTemplate = template(params);
 
-  const serverToken = process.env.EMAIL_TOKEN as string;
+  const serverToken = process.env.EMAIL_API_TOKEN as string;
   const client = new ServerClient(serverToken);
 
   client.sendEmail({
-    From: "lewis@reflr.io",
-    To: "lewis@reflr.io",
+    From: params?._from,
+    To: params?._to,
     Subject: params?._subject ?? "",
     HtmlBody: renderedTemplate,
   });
