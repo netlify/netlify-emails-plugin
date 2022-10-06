d1c98ec91c02a15d467ea0d8002dd30b5ad403d1
Lewis Thorley <lewis.thorley@netlify.com>
2022-10-06 13:02:24 +0100

    REmove files

--- src/handler/import/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.handler = void 0;
-// @ts-nocheck
-var plugin_emails_1 = require("@netlify/plugin-emails");
-exports.handler = plugin_emails_1.handler;
