1c254e2d6d1b9b7bcba48f6ba1e17342a40091c9
Lewis Thorley <lewis.thorley@netlify.com>
2022-10-10 09:51:10 +0100

    Lint staged

--- src/handler/mailer/index.ts
+++ src/handler/mailer/index.ts
@@ -24,6 +24,7 @@ interface IMailerProps {
 const mailer = async ({
   configuration,
   request,
+  test2,
 }: IMailerProps): Promise<{ statusCode: number; body: string }> => {
   const acceptedProviders = ["mailgun", "postmark", "sendgrid"];
   const emailProvider = configuration.providerName.toLowerCase();
